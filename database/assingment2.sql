69702ab43a851903fe2f0b7da176e727d7b93a30
bubba957 <scody49@gmail.com>
2025-09-13 22:33:11 -0700

    wk2

---
+++ database/assingment2.sql
@@ -0,0 +1,33 @@
+INSERT INTO public.account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    )
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1
+DELETE FROM account
+WHERE account_id = 1
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'the small interiors',
+        'a huge interior'
+    )
+SELECT inv_make,
+    inv_model,
+    classification_name
+FROM inventory
+    INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport'
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, 'ges/', 'ges/vehicles/')
+UPDATE inventory
+SET inv_thumbnail = REPLACE(inv_thumbnail, 'ges/', 'ges/vehicles/')
